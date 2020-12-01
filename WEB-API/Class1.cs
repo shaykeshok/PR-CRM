84820018f486e163ec9591d6136d30288d310583
shaykeshok <shok.shayke@gmail.com>
2020-12-01 10:44:59 +0200

    Add project files.

---
+++ WEB-API/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WEB_API
+{
+    public class Class1
+    {
+    }
+}
